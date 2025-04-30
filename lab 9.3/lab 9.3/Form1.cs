edf0c3b2096fa80d35189f63eee1ca7825f83a79
Arsen2024 <158996025+Arsen2024@users.noreply.github.com>
2025-04-30 17:57:52 +0300

    lab 9.3 C#

---
+++ lab 9.3/lab 9.3/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace lab_9._3
+{
+    public partial class Form1 : Form
+    {
+        private ParenthesesChecker checker = new ParenthesesChecker();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = "expression.txt";
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("Файл не знайдено!");
+                return;
+            }
+
+            string expression = File.ReadAllText(path);
+            textBox1.Text = expression;
+
+            var checker = new ParenthesesChecker();
+            var resultLines = checker.CheckBalance(expression);
+
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(resultLines.ToArray());
+        }
+    }
+}
---
+++ lab 9.3/lab 9.3/ParenthesesChecker.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_9._3
+{
+    public class ParenthesesChecker
+    {
+        private int[] stack;
+        private int top;
+        private List<(int openIndex, int closeIndex)> matchedPairs;
+
+        public ParenthesesChecker()
+        {
+            stack = new int[1000]; 
+            top = -1;
+            matchedPairs = new List<(int, int)>();
+        }
+
+        public List <string> CheckBalance(string expression)
+        {
+            matchedPairs.Clear();
+            top = -1;
+
+            List<string> result = new List<string>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    Push(i);
+                }
+                else if (expression[i] == ')')
+                {
+                    if (IsEmpty())
+                    {
+                        result.Add("Дужки не збалансовані: зайва закриваюча дужка на позиції " + i);
+                        return result;
+                    }
+                    int openIndex = Pop();
+                    matchedPairs.Add((openIndex, i));
+                }
+            }
+
+            if (!IsEmpty()) {
+                result.Add("Дужки не збалансовані: залишилися відкриваючі дужки.");
+                return result;
+            }
+
+            matchedPairs.Sort((a, b) => a.closeIndex.CompareTo(b.closeIndex));
+
+            result.Add("Дужки збалансовані. Пари дужок:");
+            foreach (var pair in matchedPairs) {
+                result.Add($"Відкрита на {pair.openIndex}, закрита на {pair.closeIndex}");
+            }
+
+            return result;
+        }
+
+        private void Push(int index)
+        {
+            stack[++top] = index;
+        }
+
+        private int Pop()
+        {
+            return stack[top--];
+        }
+
+        private bool IsEmpty()
+        {
+            return top == -1;
+        }
+    }
+}
