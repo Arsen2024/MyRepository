b0712ee0e6f8a76180056ca10737277a24ed103b
Arsen2024 <158996025+Arsen2024@users.noreply.github.com>
2025-04-30 17:57:16 +0300

    lab 9.1 C#

---
+++ lab 9.1/lab 9.1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace lab_9._1
+{
+    public partial class Form1 : Form
+    {
+        private List<int> L1 = new List<int>();
+        private List<int> L2 = new List<int>();
+        private List<int> L3 = new List<int>();
+
+        private ListProcessor processor = new ListProcessor();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DisplayList(ListBox listBox, List<int> list)
+        {
+            listBox.Items.Clear();
+            foreach (var item in list)
+                listBox.Items.Add(item);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            L1 = processor.ReadListFromFile("L1.txt");
+            L2 = processor.ReadListFromFile("L2.txt");
+            L3 = processor.ReadListFromFile("L3.txt");
+
+            DisplayList(listBox1, L1);
+            DisplayList(listBox2, L2);
+            DisplayList(listBox3, L3);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var modified = processor.ReplaceFirstSublist(L1, L2, L3);
+            DisplayList(listBox4, modified);
+            L1 = modified;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            processor.SaveListToFile(L1, "ModifiedL1.txt");
+            MessageBox.Show("Список збережено у файл ModifiedL1.txt");
+        }
+    }
+}
---
+++ lab 9.1/lab 9.1/ListProcessor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab_9._1
+{
+    public class ListProcessor
+    {
+        public List<int> ReadListFromFile(string path)
+        {
+            return File.ReadAllText(path)
+                       .Split(new[] { ' ', '\n', '\r', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                       .Select(int.Parse)
+                       .ToList();
+        }
+
+        public List<int> ReplaceFirstSublist(List<int> L1, List<int> L2, List<int> L3)
+        {
+            for (int i = 0; i <= L1.Count - L2.Count; i++)
+            {
+                if (L1.Skip(i).Take(L2.Count).SequenceEqual(L2))
+                {
+                    var result = new List<int>();
+                    result.AddRange(L1.Take(i));
+                    result.AddRange(L3);
+                    result.AddRange(L1.Skip(i + L2.Count));
+                    return result;
+                }
+            }
+            return L1;
+        }
+
+        public void SaveListToFile(List<int> list, string path)
+        {
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    writer.Write(list[i]);
+                    if ((i + 1) % 7 == 0)
+                        writer.WriteLine();
+                    else
+                        writer.Write(" ");
+                }
+            }
+        }
+    }
+}
